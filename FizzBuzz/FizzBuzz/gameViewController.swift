1a7d816c34bad52934103d691a8721aebc36a259
NICHOLAS JOHN SMITH <snichola@10-150-44-23.dhcp-m.deakin.edu.au>
2017-04-27 09:52:50 +1000

    Test commit. UI updated.

---
+++ FizzBuzz/FizzBuzz/gameViewController.swift
@@ -0,0 +1,35 @@
+//
+//  gameViewController.swift
+//  FizzBuzz
+//
+//  Created by NICHOLAS JOHN SMITH on 27/04/2017.
+//  Copyright © 2017 Nicholas Smith. All rights reserved.
+//
+
+import UIKit
+
+class gameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
