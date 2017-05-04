7d379862434396bfd9df0e4f1b749caf971f6099
STEPHEN BIRSA <sbirsa@10-150-45-220.dhcp-m.deakin.edu.au>
2017-05-04 13:02:12 +1000

    ~added score ~added fizz/buzz game logic ~added fail (basic, no lives yet) ~added textfields ~added Global class with static variables ~timer with counter and control speed ~main game loop ~error checking

---
+++ FizzBuzz/FizzBuzz/Global.swift
@@ -0,0 +1,17 @@
+//
+//  Global.swift
+//  FizzBuzz
+//
+//  Created by STEPHEN BIRSA on 4/05/2017.
+//  Copyright © 2017 Nicholas Smith. All rights reserved.
+//
+
+import UIKit
+
+class Global: NSObject {
+    //gameplay
+    public static var fizzNumber = 3 //default values
+    public static var buzzNumber = 5 //default values
+    public static var currentScore = 0 //default value
+    //other
+}
--- FizzBuzz/FizzBuzz/gameViewController.swift
+++ FizzBuzz/FizzBuzz/gameViewController.swift
@@ -3,33 +3,109 @@
 //  FizzBuzz
 //
 //  Created by NICHOLAS JOHN SMITH on 27/04/2017.
+//  Modified by STEPHEN BIRSA and NICHOLAS JOHN SMITH.
 //  Copyright © 2017 Nicholas Smith. All rights reserved.
 //
-
 import UIKit
-
 class gameViewController: UIViewController {
-    //testing nick991
+    //logic
+    private var counter:Int = 1 //can start at 1 or the first divisible number of fizz/buzz
+    private var buttonPressed:Bool = false
+    //timer
+    private var updateTimer:Timer?
+    private var timerSpeed:Double = 1
+    //textfields
+    @IBOutlet weak var counterTextField: UILabel!
+    @IBOutlet weak var FizzTextField: UILabel!
+    @IBOutlet weak var BuzzTextField: UILabel!
+    @IBOutlet weak var ScoreTextField: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        Global.currentScore = 0
+        counterTextField.text = "\(counter)"
+        FizzTextField.text = "\(Global.fizzNumber)"
+        BuzzTextField.text = "\(Global.buzzNumber)"
+        ScoreTextField.text = "Score: \(Global.currentScore)"
+        startTimer()
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        // Dispose of any resources before disappearing
+        stopTimer() //ends timer so timer is not continuing in another viewController
     }
-    */
-
+    private func startTimer() {
+        if updateTimer == nil {
+            updateTimer = Timer.scheduledTimer(
+                timeInterval: timerSpeed, target: self, selector: #selector(updateGame), userInfo: nil, repeats: true
+            )
+        }
+    }
+    private func stopTimer() {
+        if updateTimer != nil {
+            updateTimer?.invalidate()
+            updateTimer = nil
+        }
+    }
+    @IBAction func FizzBuzzButton(_ sender: UIButton) {
+        handleButtons(type: "fizzbuzz")
+    }
+    @IBAction func FizzButton(_ sender: UIButton) {
+        handleButtons(type: "fizz")
+    }
+    @IBAction func BuzzButton(_ sender: UIButton) {
+        handleButtons(type: "buzz")
+    }
+    private func handleButtons(type: String) {
+        buttonPressed = true
+        if (fizzBuzzLogic() == type) {
+            if (type == "fizz" || type == "buzz") { Global.currentScore += 1 }
+            if (type == "fizzbuzz") { Global.currentScore += 2 }
+            uiTracking()
+        } else {
+            uiTracking()
+            failGame()
+        }
+    }
+    private func fizzBuzzLogic() -> String {
+        //we check if there are multiples of counter
+        if (counter % Global.fizzNumber == 0 && counter % Global.buzzNumber == 0) {
+            return "fizzbuzz"
+        } else if (counter % Global.fizzNumber == 0) {
+            return "fizz"
+        } else if (counter % Global.buzzNumber == 0) {
+            return "buzz"
+        } else if (counter % Global.fizzNumber != 0 && counter % Global.buzzNumber != 0) {
+            return "nil"
+        }
+        return "nil"
+        //nil can be used for extra conditions
+        //case right now is that when its nil, you do nothing and wait for next counter value
+    }
+    //main gameplay loop
+    internal func updateGame() {
+        counterUpdate()
+        buttonPressed = false
+    }
+    private func counterUpdate() {
+        if !buttonPressed && fizzBuzzLogic() != "nil" {
+            failGame() //fail if you press nothing when a button was required
+        }
+        //otherwise succeed if you didn't need to press anything or already pressed a successful button
+        counter += 1
+        counterTextField.text = "\(counter)"
+    }
+    private func failGame() {
+        self.dismiss(animated: true, completion: nil) //takes you to previous page
+        //extension: send you to highscores page
+    }
+    private func uiTracking() {
+        ScoreTextField.text = "Score: \(Global.currentScore)"
+        //add lives checking here too
+    }
 }
